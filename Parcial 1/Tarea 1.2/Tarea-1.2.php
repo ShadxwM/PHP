2600c6be067746b2ed9a3a56519ab4cc92dac90d
ShadxwM <160081274+ShadxwM@users.noreply.github.com>
2024-02-15 17:59:08 -0600

    Add files via upload

---
+++ Parcial 1/Tarea 1.2/Tarea-1.2.php
@@ -0,0 +1,46 @@
+<html>
+<body>
+    <h1>12618. Practicando con formulas</h1> 
+    <table border="1">
+        <tr>
+            <td>Puntos</td>
+            <td>10.76</td>
+            <td>Límite de memoria</td>
+            <td>32 MiB</td>
+        </tr>
+        <tr>
+            <td>Limite de puntos (caso)</td>
+            <td>1s</td>
+            <td>Límite de tiempo (total)</td>
+            <td>1m0s</td>
+        </tr>
+        <tr>
+            <td>Tamaño límite de entrada (bytes)</td>
+            <td>10 KiB</td>
+        </tr>
+    </table>
+    <h2>Descripción</h2>
+    Dado un valor real a, quieres resolver una serie de ecuaciones hasta obtener el valor de z.
+    <img src="PracticandoConFormulas.png" alt="">
+    <h2>Entrada</h2>
+    Un real a. Puedes suponer que 1<=a<=100.
+    <h2>Ejemplo</h2>
+    <table border="1">
+        <tr>
+            <td>33.562</td>
+            <td>0.148</td>
+        </tr>
+        <tr>
+            <td>72.569</td>
+            <td>0.069</td>
+        </tr>
+        <tr>
+            <td>64.835</td>
+            <td>0.077</td>
+        </tr>
+
+    </table>
+
+
+</body>
+</html>
